e6c8bc3b092134ee017d1790427dd3e6ede9e0c0
Adi8817 <adidash560@gmail.com>
2024-02-21 21:11:12 +0530

    commited some chizs

--- com.learn.mobile/src/main/java/com/learn/mobile/adi.java
+++ com.learn.mobile/src/main/java/com/learn/mobile/adi.java
@@ -3,5 +3,6 @@ package com.learn.mobile;
 public class adi {
 public static void main(String[]args) {
 	System.out.println("Hii well come to my world ");
+	System.out.println("Hello aditya ");
 }
 }
