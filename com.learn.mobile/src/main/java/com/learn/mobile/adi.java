7fb4887b26afd8423907892df3aa6f609b35dccb
Adi8817 <adidash560@gmail.com>
2024-02-21 22:05:07 +0530

    Small commit

--- com.learn.mobile/src/main/java/com/learn/mobile/adi.java
+++ com.learn.mobile/src/main/java/com/learn/mobile/adi.java
@@ -4,5 +4,6 @@ public class adi {
 public static void main(String[]args) {
 	System.out.println("Hii well come to my world ");
 	System.out.println("Hello aditya ");
+	System.out.println("HII i am a Java developer");
 }
 }
